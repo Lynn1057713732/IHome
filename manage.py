6363c61a2e6b2c11686b186f4ecfaa824a61464f
Lynn1057713732 <1057713732@qq.com>
2018-03-30 15:25:30 +0800

    集成数据可迁移到脚本管理器

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 import redis
 
 
@@ -35,6 +36,10 @@ CSRFProtect(app)
 
 # 创建脚本管理器对象
 manager = Manager(app)
+# app和db在迁移时建立关联
+Migrate(app, db)
+# 将数据库迁移脚本添加到脚本管理器
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/', methods=['GET', 'POST'])
