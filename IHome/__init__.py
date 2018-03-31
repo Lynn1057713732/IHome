3f53cb6f3f064824ceebc4fc2eed153a2543a4c0
Lynn1057713732 <1057713732@qq.com>
2018-03-31 14:02:04 +0800

    使用了工厂模式封装了config，保证程序业务逻辑在上线后可以不用动

--- IHome/__init__.py
+++ IHome/__init__.py
@@ -4,17 +4,28 @@ from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_session import Session
-from config import Config
+from config import configs
+# 创建一个可以被外界导入的数据库连接对象，不传参数SQLAlchemy，不会调用init_app(app)，
+# 就没有db对象，在自己用到时自己调用init_app(app)，创建db对象
+db = SQLAlchemy()
+# 创建可以被外界导入的连接到redis数据库的对象
+redis_store = None
 
-app = Flask(__name__)
 
-# 加载配置
-app.config.from_object(Config)
-# 创建连接到MYSQL数据库的对象
-db = SQLAlchemy(app)
-# 创建连接到redis数据库的对象
-redis_store = redis.StrictRedis(host=Config.REDIS_IP, port=Config.REDIS_PORT,)
-# 开启csrf保护
-CSRFProtect(app)
-# 使用session在flask中的拓展
-Session(app)
+def get_app(config_name):
+    app = Flask(__name__)
+
+    # 加载配置
+    app.config.from_object(configs[config_name])
+    # 创建连接到MYSQL数据库的对象
+    # db = SQLAlchemy(app)
+    db.init_app(app)
+    # 创建连接到redis数据库的对象
+    global redis_store
+    redis_store = redis.StrictRedis(host=configs[config_name].REDIS_IP, port=configs[config_name].REDIS_PORT,)
+    # 开启csrf保护
+    CSRFProtect(app)
+    # 使用session在flask中的拓展
+    Session(app)
+
+    return app
--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     # 开启调试模式
     DEBUG = True
     # 配置MySQL数据库
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:1057713732@127.0.0.1:3306/ihome'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:1057713732@127.0.0.1:3306/ihome_dubug'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # 配置Redis数据库
     REDIS_IP = '127.0.0.1'
@@ -23,3 +23,32 @@ class Config(object):
     SESSION_USE_SIGNER = True
     # 设置session的会话的超时时长
     PERMANENT_SESSION_LIFETIME = 3600 * 24
+
+
+class DevelopmentConfig(Config):
+    """创建调试环境下的配置类"""
+    # 我们的爱家租房的房型，调试模式的配置和Config一致，所以pass
+    pass
+
+
+class ProductionConfig(Config):
+    """创建线上环境下的配置类"""
+
+    # 重写有差异性的配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.72.77:3306/ihome'
+
+
+class UnittestConfig(Config):
+    """单元测试的配置"""
+
+    # 重写有差异性的配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/iHome_test'
+
+
+# 准备工厂设计模式的原材料
+configs = {
+    'default_config': Config,
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'unittest': UnittestConfig
+}
