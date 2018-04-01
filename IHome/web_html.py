69c603885d65521e0a4b61bbdc66577afb0baf8c
Lynn1057713732 <1057713732@qq.com>
2018-04-01 17:29:40 +0800

    加载favicon.ico

--- IHome/web_html.py
+++ IHome/web_html.py
@@ -12,9 +12,11 @@ def get_static_html(file_name):
     # 需求1：http://127.0.0.1:5000/login.html
     # 需求2：http://127.0.0.1:5000/ 默认加载index.html
     # 需求3：http://127.0.0.1:5000/favicon.ico  加载title图标
+
     if not file_name:
         file_name = 'index.html'
-    # 2.拼接file_name静态文件的路径
-    file_name = 'html/' + file_name
+    if file_name != 'favicon.ico':
+        # 2.拼接file_name静态文件的路径
+        file_name = 'html/' + file_name
 
     return current_app.send_static_file(file_name)
