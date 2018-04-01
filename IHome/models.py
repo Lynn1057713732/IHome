57e0d07b64fc5e5cedb688b2cdc4642c1245813e
Lynn1057713732 <1057713732@qq.com>
2018-04-01 17:34:21 +0800

    导入response_code.py到utils,使用常量而不是魔法数字

--- IHome/models.py
+++ IHome/models.py
@@ -3,7 +3,7 @@
 
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from iHome import constants
+from IHome import constants
 from . import db
 
 
