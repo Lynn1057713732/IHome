21f09a48f004745f1096056a362a50d83fb08fd6
Lynn1057713732 <1057713732@qq.com>
2018-04-06 17:50:23 +0800

    发布房源input和checked数据接口

--- IHome/api_1_0/houses.py
+++ IHome/api_1_0/houses.py
@@ -2,9 +2,82 @@
 # 用户房屋模块接口
 
 from . import api
-from flask import current_app, jsonify
+from flask import current_app, jsonify, request, g, session
 from IHome.utils.response_code import RET
-from IHome.models import Area
+from IHome.models import Area, House, Facility
+from IHome import db, constants, redis_store
+from IHome.utils.common import login_required
+
+
+@api.route('/houses', methods=['POST'])
+@login_required
+def pub_house():
+    """发布新房源
+    0.校验用户是否登录
+    1.接受参数
+    2.校验参数
+    3.实例化房屋模型对象，给对象赋值属性
+    4.保存到数据库
+    5，返回响应信息
+    """
+    # 1.接受所有参数
+    json_dict = request.json
+
+    title = json_dict.get('title')
+    price = json_dict.get('price')
+    address = json_dict.get('address')
+    area_id = json_dict.get('area_id')
+    room_count = json_dict.get('room_count')
+    acreage = json_dict.get('acreage')
+    unit = json_dict.get('unit')
+    capacity = json_dict.get('capacity')
+    beds = json_dict.get('beds')
+    deposit = json_dict.get('deposit')
+    min_days = json_dict.get('min_days')
+    max_days = json_dict.get('max_days')
+
+    # 2.校验参数：注意price / deposit， 需要用户传入数字
+    # 判断参数是否缺失
+    if not all([title, price, address, area_id, room_count, acreage, unit, capacity, beds, deposit, min_days, max_days]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数缺失")
+    # 提示：在开发中，对于像价格这样的浮点数，不要直接保存浮点数，因为有精度的问题，一般以分为单
+    try:
+        price = int(float(price) * 100)
+        deposit = int(float(deposit) * 100)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数格式错误")
+
+    # 3.实例化房屋模型对象，并给房屋属性赋值
+    house = House()
+    house.user_id = g.user_id
+    house.area_id = area_id
+    house.title = title
+    house.price = price
+    house.address = address
+    house.room_count = room_count
+    house.acreage = acreage
+    house.unit = unit
+    house.capacity = capacity
+    house.beds = beds
+    house.deposit = deposit
+    house.min_days = min_days
+    house.max_days = max_days
+
+    # 处理房屋的设施，保存设置的表示数字
+    facilities = json_dict.get('facility')
+    # 查询出被选中的设置模型对象，赋值给房子的模型属性
+    house.facilities = Facility.query.filter(Facility.id.in_(facilities)).all()
+    # 4.保存到数据库
+    # try:
+    #     db.session.add(house)
+    #     db.session.commit()
+    # except Exception as e:
+    #     current_app.logger.error(e)
+    #     db.session.rollback()
+    #     return jsonify(errno=RET.DBERR, errmsg="发布新房源失败")
+    # 5.响应结果
+    return jsonify(errno=RET.OK, errmsg="发布新房源成功", data={'house_id':house.id})
 
 
 @api.route('/areas')
--- IHome/static/js/ihome/newhouse.js
+++ IHome/static/js/ihome/newhouse.js
@@ -22,7 +22,50 @@ $(document).ready(function(){
     })
 
     // TODO: 处理房屋基本信息提交的表单数据
+    $('#form-house-info').submit(function (event) {
+        event.preventDefault();
+        //准备参数
+        var params = {};
 
+        //收集房屋input标签数据
+        //serializeArray()收集form表单中需要提交的input标签，收集到一个数组对象中
+        // map 遍历对象，比如说数组对象
+        // obj == {name: "title", value: "1"}
+        $(this).serializeArray().map(function (obj) {
+            params[obj.name] = obj.value;
+        });
+
+        //收集房屋的设施信息checkbox,不加这段那么显示的facility只有最后一个
+        //  $(':checkbox:checked[name=facility]') == 收集界面上被选中的name为facility的checkbox
+        // each 遍历界面上收集到的标签
+        // elem == <input type="checkbox" name="facility" value="2">热水淋浴
+        facilities = [];
+        $(':checkbox:checked[name=facility]').each(function (i, elem) {
+            // console.log(elem);
+            facilities[i] = elem.value;
+        });
+        params['facility'] = facilities;
+
+        //发送房屋数据给后端，ajax
+        $.ajax({
+            url:'/api/1.0/houses',
+            type:'post',
+            data:JSON.stringify(params),
+            contentType:'application/json',
+            headers:{'X-CSRFToken':getCookie('csrf_token')},
+            success:function (response) {
+                if(response.errno == '0'){
+
+                }
+                else if (response.errno == '4101') {
+                    location.href = '/';
+                }
+                else{
+                    alert(response.errmsg)
+                }
+            }
+        })
+    })
     // TODO: 处理图片表单的数据
 
 })
